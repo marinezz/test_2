d726626cbf0612254dc2007883f7f94b0e3d989f
marine <1075562207@qq.com>
2023-02-05 14:39:24 +0800

    v3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! v1");
+        System.out.printf("hello v2");
+        System.out.printf("hello v3");
     }
 }
