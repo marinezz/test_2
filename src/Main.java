8ec917753215a9bb87e2e977b5025119c283080d
marine <1075562207@qq.com>
2023-02-05 15:07:10 +0800

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.printf("hello v2");
         System.out.printf("hello v3");
         System.out.printf("hello v4");
+        System.out.printf("bug fix");
     }
 }
